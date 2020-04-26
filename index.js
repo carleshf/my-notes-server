bd309be52e6f1b119e23d37c48120a9d8ce513ac
Carles <kuragari.ch@gmail.com>
2020-04-26 11:55:24 +0200

    Updated examples to Express.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http')
+/*const http = require('http')
 const port = process.env.PORT || 3000
 
 const winston = require('winston');
@@ -38,4 +38,42 @@ server.listen(port, (err) => {
         return console.log('something bad happened', err)
     }
     console.log(`server is listening on ${port}`)
+})*/
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa')
+const shortid = require('shortid')
+const Promise = require('bluebird')
+const DAO = require('./dao')
+const Notes = require('./notes')
+
+// define the Express app
+const app = express()
+
+// enhance your app security with Helmet
+app.use(helmet())
+
+// use bodyParser to parse application/json content-type
+app.use(bodyParser.json())
+
+// enable all CORS requests
+app.use(cors())
+
+// log HTTP requests
+app.use(morgan('combined'))
+
+
+app.get('/', (req, res) => {
+    res.send('hello moon')
+})
+app.get('/about', (req, res) => {
+    res.send('<h1>about</h1>')
+})
+app.get('/contact', (req, res) => {
+    res.send('<h1>contact</h1>')
 })
